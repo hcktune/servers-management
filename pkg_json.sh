dce881c75a7b1b41a27417d8346057b4acce46a6
hcktune <aymane-hana@hotmail.fr>
2024-06-05 00:22:31 +0100

    optimize files, setting hosts to "all"

--- pkg_json.sh
+++ pkg_json.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-jq '.plays[].tasks.[1].hosts |  to_entries[] | {ip: .key, changed: select(.key).value.changed, update: select(.key).value.update, state: select(.key).valued.state, file_to_deb: select(.key).value.deb, upgrade: select(.key).value.upgrade, autoclean: select(.key).value.autoclean, autoremove: select(.key).value.autoremove, purge: select(.key).value.purge, msg: select(.key).value.msg}'
+jq '
+  .plays[].tasks[1].hosts 
+  | to_entries[] 
+  | {
+      ip: .key, 
+      changed: .value.changed, 
+      update: .value.update, 
+      state: .value.state, 
+      file_to_deb: .value.deb, 
+      upgrade: .value.upgrade, 
+      autoclean: .value.autoclean, 
+      autoremove: .value.autoremove, 
+      purge: .value.purge, 
+      msg: .value.msg
+    }
+'
--- user_json.sh
+++ user_json.sh
@@ -1,3 +1,19 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-jq '.plays[].tasks.[1].hosts |  to_entries[] | {ip: .key, password: select(.key).value.invocation.module_args.password, user: select(.key).value.name,  changed: select(.key).value.changed, state: select(.key).value.state, uid: select(.key).value.uid,  gid: select(.key).value.group, groups: select(.key).value.groups, sysUser: select(.key).value.invocation.module_args.system, msg: select(.key).value.msg}'
+# Process the hosts section
+jq '
+  .plays[].tasks[1].hosts 
+  | to_entries[] 
+  | {
+      ip: .key, 
+      changed: .value.changed, 
+      update: .value.update, 
+      state: .value.state, 
+      file_to_deb: .value.deb, 
+      upgrade: .value.upgrade, 
+      autoclean: .value.autoclean, 
+      autoremove: .value.autoremove, 
+      purge: .value.purge, 
+      msg: .value.msg
+    }
+' 
