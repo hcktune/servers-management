64032387d25305af6c736aa49c12285d9360558f
hcktune <aymane-hana@hotmail.fr>
2024-06-05 04:16:09 +0100

    adding  stderr and stdlines to the json process files

--- pkg_json.sh
+++ pkg_json.sh
@@ -6,13 +6,15 @@ jq '
   | {
       ip: .key, 
       changed: .value.changed, 
-      update: .value.update, 
+      update: .value.update,
       state: .value.state, 
       file_to_deb: .value.deb, 
       upgrade: .value.upgrade, 
       autoclean: .value.autoclean, 
       autoremove: .value.autoremove, 
       purge: .value.purge, 
+      stderr: .value.stderr,
+      stdout_lines: .value.stdout_lines,
       msg: .value.msg
     }
 '
